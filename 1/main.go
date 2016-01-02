457051e9d9f1838e259df271421b80d5a33b1a78
Pedro Luz <narvenblog@gmail.com>
2016-01-02 19:49:17 +0000

    Problem 1 Multiples of 3 and 5

---
+++ 1/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var total int
+
+	for i := 1; i < 1000; i++ {
+
+		if i%3 == 0 {
+			total += i
+		} else if i%5 == 0 {
+			total += i
+		}
+
+	}
+
+	fmt.Println(total)
+}
